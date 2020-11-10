0abd1b8e78ccb4499bf67f20a9eaaed1042e0b8d
Logan Murphy <loganrjmurphy@gmail.com>
2020-11-10 12:10:20 -0500

    Initial LTL

--- src/LTL.lean
+++ src/LTL.lean
@@ -1,5 +1,5 @@
 import data.stream tactic
-open tactic 
+open stream tactic 
 
 namespace LTL 
 
@@ -23,11 +23,11 @@ namespace formula
 def disj (φ ψ : formula AP) :formula AP := 
 ∼ (∼φ & ∼ψ)
 
-local infix ` + ` :60 := formula.disj _
+local infix ` ⅋ ` :60 := formula.disj _
 
 
 def impl (φ ψ : formula AP) : formula AP:= 
-∼φ + ψ
+∼φ ⅋ ψ
 
 local infix ` ⇒` : 50 := formula.impl _
 
@@ -60,7 +60,7 @@ def sat :  inf_word AP → formula AP → Prop
 notation  σ ` ⊨ ` φ := sat _ σ φ 
 
 def disj (φ ψ : formula AP) (σ : inf_word AP) : 
-    (σ ⊨ φ + ψ) ↔ (σ ⊨ φ) ∨ (σ ⊨ ψ) := 
+    (σ ⊨ φ ⅋ ψ) ↔ (σ ⊨ φ) ∨ (σ ⊨ ψ) := 
 begin
      rw disj, repeat {rw sat},
      rw [not_and, not_not,imp_iff_not_or, not_not],
@@ -83,7 +83,47 @@ begin
       split, {exact H}, {intros, trivial}} 
 end 
 
-def always (φ : formula AP) (σ : inf_word AP)
+def always (φ : formula AP) (σ : inf_word AP) : 
+    (σ ⊨ ◾ φ) ↔ ∀ i, σ[i..] ⊨ φ := 
+begin
+    rw always,
+    rw [sat, sat.eventually, not_exists],
+    split,
+    {intros H i, replace H := H i, rwa [sat, not_not] at H},
+    {intros H i, rw sat, rw not_not, apply H i}
+end  
+
+def always_eventually (φ : formula AP) (σ : inf_word AP) : 
+    (σ ⊨ ◾(◆φ)) ↔ ∀ i, ∃ j, j ≥ i ∧ (σ[j..] ⊨ φ):= 
+begin
+    rw always, split,
+    { intros H i, replace H := H i, rw eventually at H,
+      cases H with k H, rw drop_drop at H,
+      use (k+i),
+      split,
+      {rw [ge_iff_le,le_add_iff_nonneg_left], apply zero_le},
+      exact H},
+    { intros H i, replace H := H i, rcases H with ⟨k,H1,H2⟩,
+      rw eventually, use (k-i),
+      rw drop_drop, rwa nat.sub_add_cancel H1,}
+end  
+
+def eventually_always (φ : formula AP) (σ : inf_word AP) : 
+    (σ ⊨ ◆(◾φ)) ↔ ∃ i, ∀ j, j ≥ i → (σ[j..] ⊨ φ):= 
+begin
+    rw eventually, split,
+    { rintros ⟨k,H⟩, rw always at H,
+      use k, intros j H',
+      replace H := H (j-k),
+      rw drop_drop at H,
+      rwa nat.sub_add_cancel H' at H},
+    { rintros ⟨k,H1⟩,
+      use k,rw always, intro i,
+      replace H1 := H1 (i+k),
+      rw drop_drop, apply H1, 
+      rw [ge_iff_le, le_add_iff_nonneg_left], apply zero_le}
+end  
+
 
 
 
